af03782356996cee1d3ad5f6f1cd4b15a9640041
DevkotaSaksham <sakxamdev@gmail.com>
2021-11-19 15:37:28 +0545

    without networking

---
+++ multiP/Assets/assets/scripts/animationcontroller.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class animationcontroller : MonoBehaviour
+{
+
+    Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+        animator = GetComponent<Animator>();
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("w"))
+        {
+            animator.SetBool("iswalking", true);
+        }
+        if (!Input.GetKey("w"))
+        {
+            animator.SetBool("iswalking", false);
+        }
+        //for right walk
+        if (Input.GetKey("d"))
+        {
+            animator.SetBool("isrightwalking", true);
+        }
+        if (!Input.GetKey("d"))
+        {
+            animator.SetBool("isrightwalking", false);
+        }
+        //for left walking
+        if (Input.GetKey("a"))
+        {
+            animator.SetBool("isleftwalking", true);
+        }
+        if (!Input.GetKey("a"))
+        {
+            animator.SetBool("isleftwalking", false);
+        }
+        //for back walking
+        if (Input.GetKey("s"))
+        {
+            animator.SetBool("isbackwalking", true);
+        }
+        if (!Input.GetKey("s"))
+        {
+            animator.SetBool("isbackwalking", false);
+        }
+
+        //forjump
+        if (Input.GetKey(KeyCode.Space))
+        {
+            animator.SetBool("isjumping", true);
+        }
+        if (!Input.GetKey(KeyCode.Space))
+        {
+            animator.SetBool("isjumping", false);
+        }
+        //forkicking
+        if (Input.GetKey("e"))
+        {
+            animator.SetBool("iskicking", true);
+        }
+        if (!Input.GetKey("e"))
+        {
+            animator.SetBool("iskicking", false);
+        }
+    }
+}
---
+++ multiP/Assets/assets/scripts/playercontroller.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playercontroller : MonoBehaviour
+{
+    
+    Rigidbody rb;
+
+    public float speed = 10f;
+    void Start()
+    {
+       
+        rb = GetComponent<Rigidbody>();
+       
+        
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        var dir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        // transform.Translate(dir * speed * Time.deltaTime);
+        rb.velocity = dir * speed;
+        //forwalking
+        
+    }
+}
